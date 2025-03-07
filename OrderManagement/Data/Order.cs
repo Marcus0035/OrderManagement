0242a69fd1acf188698c4a34a4dc398d21d7ee77
Marcus0035 <marek.dvoracek606@gmail.com>
2025-03-07 08:00:21 +0100

    AddModels

---
+++ OrderManagement/Data/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderManagement.Data
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        //Items, Quantity
+        public Dictionary<OrderItem, int> OrderItems = new();
+        public Customer Customer { get; set; } = new();
+        public DateOnly OrderDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    }
+}
---
+++ OrderManagement/Data/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagement.Data
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public List<Order> Orders { get; set; } = new();
+    }
+}
