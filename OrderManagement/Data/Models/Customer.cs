cc58ca0650c8404017b45106d113be4f8a5bd751
Marcus0035 <marek.dvoracek606@gmail.com>
2025-03-07 09:15:55 +0100

    AddDeletedPropertyToCustomer

---
+++ OrderManagement/Data/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderManagement.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public List<Order> Orders { get; set; } = new();
+        public bool Deleted { get; set; } = false;
+    }
+}
